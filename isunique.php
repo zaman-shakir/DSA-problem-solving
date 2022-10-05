9db3b53d3737c6f107bfbce75c0267739b325b80
Shakir Uz Zaman <shakir@grype.ca>
2022-10-05 14:06:27 +0600

    solution 2

--- isunique.php
+++ isunique.php
@@ -11,16 +11,32 @@ function is_unique(array $arr =[]){
     * Solution 1
     * Starts
     */
-    if(empty($arr)){
-        return false;
-    }
-    if(count($arr) == count(array_flip($arr))){
-         return true;
-    }
-    else return false;
+    // if(empty($arr)){
+    //     return false;
+    // }
+    // if(count($arr) == count(array_flip($arr))){
+    //      return true;
+    // }
+    // else return false;
 
+    /**
+     * Soluttion 2
+     * Strat
+    */
+    $new_arr =[];
+    foreach($arr as $a ){
+        if($new_arr[$a])
+        {
+            $new_arr[$a] = false;
+            return false;
+        }
 
+        else $new_arr[$a] = true;
+     }
+     return true;
 }
 $arr = [1,2,3,4,5,6,7,8,8,9,9,7,3,2];
 $arr = [9,7,4,6];
+$arr = ['a','c','d','h','a'];
+$arr = ['a','c','d','h'];
 var_dump(is_unique($arr));
