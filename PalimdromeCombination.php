884a7ccb1175670d1c1a42253d2c104afbf62203
Shakir Uz Zaman <shakir@grype.ca>
2022-10-07 10:20:02 +0600

    1.4

--- PalimdromeCombination.php
+++ PalimdromeCombination.php
@@ -13,6 +13,32 @@
 */
 
 function palindromePermutation (string $str): bool{
+    // remove non letter
+    $char_map = [];
+    $odd_count = 0;
+    for($i=0; $i<strlen($str); $i++){
+        $char = $str[$i];
 
+        if($char === " ") continue;
 
+        $char = strtolower($char);
+
+        if( array_key_exists($char, $char_map)){
+            $char_map[$char]++;
+        }
+        else{
+            $char_map[$char] = 1;
+        }
+    }
+    foreach($char_map as $key=>$count){
+
+        if($count % 2 != 0 && ++$odd_count > 1 ){
+            return false;
+        }
+    }
+
+    //var_dump($str);
+    return true;
 }
+
+var_dump(palindromePermutation("aaccde"));
