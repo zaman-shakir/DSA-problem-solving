513580e2cb2f6c1a7b7e7cd6ecf7e250ed9c0e83
Shakir Uz Zaman <shakir@grype.ca>
2022-10-07 08:55:23 +0600

    1.4

---
+++ PalimdromeCombination.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author  shakir
+ * @brief   Checkif the sring is permutation of a palimdrome
+ *
+ *
+ * @param   $str    string
+ *
+ * @return   true    if palindrome combination
+ * @return  false   if not
+ *
+ * @version 1.0.0   Oct 7, 20222
+*/
+
+function palindromePermutation (string $str): bool{
+
+
+}
