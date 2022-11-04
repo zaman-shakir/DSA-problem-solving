ca6eb8e05ee4a761b1d772cd1d43f5a76b1f9fe3
Shakir Uz Zaman <shakir@grype.ca>
2022-11-04 18:02:19 +0600

    longestUniquesubstr is done

--- longestUniqueSubsttr.php
+++ longestUniqueSubsttr.php
@@ -1,4 +1,5 @@
 <?php
+/*
 function hasUnique($str,$i,$k){
 
     $hash = [];
@@ -28,8 +29,28 @@ function longestUniqueSubsttr($str){
     return $max;
 }
 
+*/
+function longestUniqueSubsttr($s){
+    $len = strlen($s);
+    if($len == 0) return 0;
+    if($len == 1) return 1;
+    $hash = [];
+    $maxLength = 0;
+    $start = 0;
+    for($i=0; $i<$len; $i++){
+        $char = $s[$i];
+        if(array_key_exists($char,$hash)){
+            $start = max($start, $hash[$char]+1);
+        }
+        $hash[$char] = $i; // set/override current position
+        $maxLength = max($maxLength, $i-$start+1);
+    }
+    return $maxLength;
 
+}
 $str = "geeksforgeeks";
+$str = "abcabcbb";
+$str = "au";
 var_dump(longestUniqueSubsttr($str));
 
 
