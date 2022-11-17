407ae9b83c3aeb59d7241df5f99ed64882f142eb
Shakir Uz Zaman <shakir@grype.ca>
2022-11-17 12:48:47 +0600

    bst started

---
+++ bts.php
@@ -0,0 +1,40 @@
+<?php
+Class Node{
+    public $info;
+    public $left;
+    public $right;
+    public $level;
+    public function __construct($info = null)
+    {
+        $this->info = $info;
+        $this->left = null;
+        $this->right = null;
+        $this->level = null;
+    }
+    public function __toString()
+    {
+        return "$this->info";
+    }
+}
+//$n1 = new Node(5);
+Class BST{
+    public $root;
+    public function __construct()
+    {
+        $this->root = null;
+    }
+
+    public function create($info){
+
+    }
+
+
+
+}
+
+
+
+
+
+
+?>
