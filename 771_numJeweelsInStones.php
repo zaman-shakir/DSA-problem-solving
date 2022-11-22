6ed3b9bbef65ff11d4a4a95a473465798ad0c6a2
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2022-11-22 14:58:59 +0600

    771 is done using brute force

---
+++ 771_numJeweelsInStones.php
@@ -0,0 +1,14 @@
+/**
+     * @param String $jewels
+     * @param String $stones
+     * @return Integer
+     */
+    function numJewelsInStones($jewels, $stones) {
+        $ans = 0;
+        for($i =0; $i<strlen($jewels); $i++){
+            for($j=0; $j<strlen($stones); $j++){
+                if($jewels[$i] == $stones[$j]) $ans++;
+            }
+        }
+        return $ans;
+    }
