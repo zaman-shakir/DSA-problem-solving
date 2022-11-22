a571f415f612f8de0968ea501ebe27f0a1c04499
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2022-11-23 00:08:10 +0600

    done

--- 1365_smallerNumbersThanCurrent.php
+++ 1365_smallerNumbersThanCurrent.php
@@ -11,11 +11,13 @@
         foreach($nums as $num){
             $hash[$num]++;
         }
+
         for($i=1; $i<count($hash);$i++){
             $hash[$i] += $hash[$i-1];
         }
+        print_r($hash);
         foreach($nums as $n){
-            if($n == 0) $ans[] = $hash[0];
+            if($n == 0) $ans[] = 0;
             else{
                 $ans[] = $hash[(int)$n-1];
             }
@@ -25,5 +27,6 @@
     }
 
     $input = [8,1,2,2,3];
+    $input = [5,0,10,0,10,6];
     $res = smallerNumbersThanCurrent($input);
     var_dump($res);
