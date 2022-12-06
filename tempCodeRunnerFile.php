b7060af385dbb6e1fbb65a20402ff7f6f130627a
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2022-12-07 01:53:43 +0600

    updated

--- tempCodeRunnerFile.php
+++ tempCodeRunnerFile.php
@@ -1,5 +1,5 @@
 <?php
-if(  abs( ($nums[$i]/ ($i+1)) - (($nums[$len-1]- $nums[$i])/ ($len-1-$i) ) ) < $min  ) {
-            //     $min = abs($nums[$i] - ($nums[$len]- $nums[$i]));
-            //     $position = $i;
-            // }
+if($id > 100000){
+            echo $id ;
+            break;
+        }
--- test.php
+++ test.php
@@ -1,13 +1,23 @@
 <?php
-$link = "https://www.hilltimes.com/story/2022/05/30/as-feds-spark-ev-demand-advocates-charge-for-same-focus-on-transit-other-vehicle-alternatives/230218/?i=63032803aa674dde96c2a5ee9b70fdbd";
-$get_url= "http://newdev3.hilltimes.com/2021/06/16/as-liberals-trumpet-merits-of-nsicop-opposition-mps-want-supremacy-of-parliament-respected/301919/?i=63032803aa674dde96c2a5ee9b70fdbd";
-$link_array = explode('/',$get_url);
-$post_old_id = end($link_array);
-var_dump($link_array[count($link_array)-2]);
-if(isset($_GET['i'])){
-    // do operation
+$url[] = "http://newdev3.hilltimes.com/?post_type=news_opinion_list_pc&p=355168&preview=true";
+$url[] = "https://prod4.hilltimes.com/story/2022/12/05/357637/357637/";
+$url[] = "https://prod4.hilltimes.com/story/2022/12/07/the-north-needs-less-ottawa%ef%bf%bc/357844/?preview=true";
+$url[] = "https://prod4.hilltimes.com/story/2022/12/07/the-north-needs-less-ottawa%ef%bf%bc/357844/?preview_id=357844&preview_nonce=d2c445ec50&preview=true&_thumbnail_id=357848";
+
+foreach($url as $link){
+
+    preg_match_all('!\d+\.*\d*!', $link, $output);
+    $output = $output[0];
+    //var_dump($output);
+    for($i=0; $i<count($output); $i++){
+
+        $id = (int) $output[$i];
+        //var_dump($id);
+        if($id > 100000){
+            echo $id. "\n" ;
+            break;
+        }
+    }
+    echo "\n";
+
 }
-
-
-
-?>
