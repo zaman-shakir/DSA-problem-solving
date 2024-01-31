b620c0387b70b8bef347624cd6cd290b4896a014
Shakir Uz Zaman <zaman.shakirdev@gmail.com>
2024-01-31 12:15:41 +0600

    leetocde 5

---
+++ 5_Longest_Palindromic_Substring.php
@@ -0,0 +1,36 @@
+<?php
+@category : leetcode No 5 problem solve
+/**
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+
+Constraints:
+
+1 <= s.length <=1000 s consist of only digits and English letters.
+ */
+
+/**
+ * Function will check the longest palindrome string inside a given string
+ * @param  String $s //String to bec hecked for palindrome
+ * @return  String
+ */
+function longestPalindrome($s)
+{
+
+    $length = strlen($s);
+
+    if($length == 1)
+        return $s;
+    for($i=0; $i<$length; $i++){
+
+    }
+
+}
